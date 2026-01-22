554d5ac05ecb1b8bd575ba86b79b0fad27d57988
nicholas lau <nicholaslau@MacBook-Air-2.local>
2026-01-22 09:25:22 +0200

    Add bot code and dotenv

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const http = require("http");
+
+// Basic server so Render doesn't timeout
+const PORT = process.env.PORT || 3000;
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Bot is running!\n");
+}).listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
 require('dotenv').config(); // loads .env
 
 const { 
